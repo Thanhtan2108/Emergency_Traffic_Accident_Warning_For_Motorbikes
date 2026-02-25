2b8f3b7ae7424718a6644bcd597fdb9db1a0ee19
Thanhtan2108 <Nthanhtan2004@gmail.com>
2026-02-25 17:43:52 +0700

    prepare document

---
+++ lib/Common/Common.h
@@ -0,0 +1,10 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define MPU6050_ADDR 0x68
+
+#define I2C_SDA_PIN    21
+#define I2C_SCL_PIN    22
+#define I2C_FREQ       400000 // 400kHz for I2C Fast mode 
+
+#endif
