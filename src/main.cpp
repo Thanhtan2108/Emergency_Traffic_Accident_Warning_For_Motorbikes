e56992deaccaae98716b05bcbdbce0ad98f06936
Thanhtan2108 <Nthanhtan2004@gmail.com>
2026-03-05 22:24:41 +0700

    test function: test code to read data accelTotal and angleX, angleY

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+#include <ReadDataFromSensor.h>
+
+ReadData ReadDataSensor;
 
 void setup() {
+    Serial.begin(115200);
 
+    ReadDataSensor.begin();
 }
 
-void loop() {
+void loop()
+{
+    ReadDataSensor.update();
 
+    float A = ReadDataSensor.getAccelTotal();
+    float angleX = ReadDataSensor.getAngleX();
+    float angleY = ReadDataSensor.getAngleY();
+
+    Serial.print("AccelTotal: ");
+    Serial.println(A);
+
+    Serial.print("AngleX: ");
+    Serial.println(angleX);
+
+    Serial.print("AngleY: ");
+    Serial.println(angleY);
+    Serial.println("========================================");
+
+    delay(1000);
 }
